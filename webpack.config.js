5d55842f037464e5267ca6fab67760b93b0f8ac4
PululuK <pululuandre@hotmail.com>
2022-11-05 02:30:25 +0100

    Introduce assets handler

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('assets/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/_dev/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
