924078e9d50eedc0f161e01c53ea620b90e163ab
PululuK <pululuandre@hotmail.com>
2022-11-11 02:16:13 +0100

    Add form handler

--- src/App/App.php
+++ src/App/App.php
@@ -2,6 +2,8 @@
 
 namespace Prestainfra\PsInstanceCreator\App;
 
+use Exception;
+
 final class App
 {
     protected $dockerClient;
@@ -17,6 +19,13 @@ final class App
 
     public function renderView(): string
     {
+        if ($this->isFormSubmit('submitForm')) {
+            $handleFormResult = $this->handleForm();
+            return $this->templateEngine->render('form_result', [
+                'response' => $handleFormResult
+            ]);
+        }
+
         return $this->templateEngine->render('index', $this->getViewVariables());
     }
 
@@ -25,6 +34,36 @@ final class App
         return [
             'assets_dir' => _APP_ASSETS_DIR_,
             'ps_docker_images' => $this->dockerClient->getPrestashopImages()
+        ];
+    }
+
+    protected function isFormSubmit(string $submitName): bool
+    {
+        return isset($_POST[$submitName]) || isset($_GET[$submitName]);
+    }
+
+    protected function getFormValue(string $key)
+    {
+        return $_POST[$key] ?? $_GET[$key];
+    }
+
+    protected function handleForm(): array
+    {
+        $messages = [];
+
+        try {
+            $this->dockerClient->createPrestaShopInstance($this->getContainerOption());
+        } catch (Exception  $e) {
+            $messages[] = $e->getMessage();
+        }
+        return $messages;
+    }
+
+    protected function getContainerOption(): array
+    {
+        return [
+            'image_id' => $this->getFormValue('image_id'),
+            'ports' => $this->getFormValue('ports'),
         ];
     }
 }
