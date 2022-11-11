678273ee9c357405b9031730f06bde5f08ffd4c2
PululuK <pululuandre@hotmail.com>
2022-11-11 02:12:30 +0100

    Introduce container creator options resolver

--- src/Adapter/Docker/DockerClient.php
+++ src/Adapter/Docker/DockerClient.php
@@ -2,12 +2,16 @@
 
 namespace Prestainfra\PsInstanceCreator\Adapter\Docker;
 
+use Docker\API\Model\ContainersCreatePostBody;
 use Prestainfra\PsInstanceCreator\App\DockerClientInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Docker\Docker;
+use Docker\API\Client as DockerApiClient;
 
 class DockerClient implements DockerClientInterface
 {
-    protected $dockerClient;
+    protected DockerApiClient $dockerClient;
+    protected array $options;
 
     public function __construct()
     {
@@ -17,12 +21,11 @@ class DockerClient implements DockerClientInterface
     public function getPrestashopImages(): array
     {
         $dockerImages = $this->dockerClient->imageList();
+        $psDockerImages = [];
 
         if (empty($dockerImages)) {
             return $psDockerImages;
         }
-
-        $psDockerImages = [];
 
         foreach ($dockerImages as $dockerImage) {
             $psDockerImages[$dockerImage->getId()] = $dockerImage->getRepoTags()[0];
@@ -33,6 +36,34 @@ class DockerClient implements DockerClientInterface
 
     public function createPrestaShopInstance(array $options): array
     {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+
+        $this->options = $resolver->resolve($options);
+
+        $containerConfig = new ContainersCreatePostBody();
+        $containerConfig->setImage($resolver->offsetGet('image_id'));
+
+        $containerConfig->setTty($resolver->offsetGet('tty'));
+        $containerConfig->setAttachStdin($resolver->offsetGet('stdin'));
+        $containerConfig->setAttachStdout($resolver->offsetGet('stdout'));
+        $containerConfig->setAttachStderr($resolver->offsetGet('stderr'));
+
         return [];
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setRequired(['image_id', 'ports']);
+
+        $resolver->setDefaults([
+            'stdin' => true,
+            'stdout' => true,
+            'stderr' => true,
+            'tty' => true,
+        ]);
+
+        $resolver->setAllowedValues('image_id', 'string');
+        $resolver->setAllowedValues('ports', 'array');
     }
 }
